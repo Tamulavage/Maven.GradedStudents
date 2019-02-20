c781f11681f8edf2de9c5cd69fd8bf3b49246b50
David Tamulavage <davidtamulavage@zipcoders-MBP.fios-router.home>
2019-02-20 16:49:21 -0500

    inital Student class and unit test

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,38 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private  ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double... testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (testScores != null) {
+            this.examScores = new ArrayList<>((Arrays.asList(testScores)));
+        }
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,71 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void constructorTest(){
+        // Given
+        String expectedFirstName = "Bob";
+        String expectedLastName = "Dole";
+        Double[] testScores = new Double[]{0.0, 13.0 , 66.5 };
+
+
+        // When
+        Student studentTest = new Student(expectedFirstName,expectedLastName, testScores);
+        String actualFName = studentTest.getFirstName();
+        String actualLName = studentTest.getLastName();
+
+        // Then
+        Assert.assertEquals(expectedFirstName, actualFName);
+        Assert.assertEquals(expectedLastName, actualLName);
+
+    }
+
+    @Test
+    public void getSetFirstNameTest(){
+        // Given
+        String expectedFirstName = "Not Me";
+        Student studentTest = new Student(null,null, null);
+
+        // When
+        studentTest.setFirstName(expectedFirstName);
+        String actualFName = studentTest.getFirstName();
+
+        // Then
+        Assert.assertEquals(expectedFirstName, actualFName);
+
+    }
+
+    @Test
+    public void getSetLastNameTest(){
+        // Given
+        String expectedLasttName = "Not My Last Name";
+        Student studentTest = new Student(null,null, null);
+
+        // When
+        studentTest.setLastName(expectedLasttName);
+        String actualLName = studentTest.getLastName();
+
+        // Then
+        Assert.assertEquals(expectedLasttName, actualLName);
+
+    }
+
+    @Test
+    public void getNumberOfExamsTakenTest(){
+        // Given
+        Double[] testScores = new Double[]{0.0, 13.0 , 66.5 };
+        Integer expected = 3;
+        Student studentTest = new Student(null,null, testScores);
+
+        // When
+        Integer actual = studentTest.getNumberOfExamsTaken();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
