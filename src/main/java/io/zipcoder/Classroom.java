0c27082100565c6c5553bfa4288c59cab7495d67
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-28 13:50:52 -0500

    Classroom and Student class and test cases

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,19 +1,21 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Classroom {
     private Student[] students;
 
-    public Classroom(int maxNumberOfStudents){
+    public Classroom(int maxNumberOfStudents) {
         this.students = new Student[maxNumberOfStudents];
 
     }
 
-    public Classroom(Student[] students){
+    public Classroom(Student[] students) {
         this.students = students;
 
     }
 
-    public Classroom(){
+    public Classroom() {
         this.students = new Student[30];
     }
 
@@ -25,20 +27,109 @@ public class Classroom {
         return this.students.length;
     }
 
+    public Integer getActiveStudentCountInClass() {
+        int countOfActiveStudents = 0;
+        for (Student ele : students) {
+            if (ele != null) {
+                countOfActiveStudents++;
+            }
+        }
+        return countOfActiveStudents;
+    }
+
     public double getAverageExamScore() {
-        Integer studentCount = getStudentsCountInClass();
+        Integer studentCount = getActiveStudentCountInClass();
+        Double sum = 0.0;
+        for (Student ele : students) {
+            if (ele != null) {
+                sum = sum + ele.getAverageExamScore();
+            }
+        }
 
-        //Double sumOfExams =
-
-        return 0;
+        return sum / studentCount;
     }
 
 
     public void add(Student newStudent) {
+        for (int i = 0; i <= getActiveStudentCountInClass(); i++) {
+            if (students[i] == null) {
+                students[i] = newStudent;
+                i = getActiveStudentCountInClass();
+            }
+        }
 
     }
 
     public void removeStudent(String fName, String lName) {
+        Integer currentActiveStudentsIndex = getActiveStudentCountInClass() - 1;
+        if (currentActiveStudentsIndex == 0 && fName.equals(students[0].getFirstName()) && lName.equals((students[0].getLastName()))) {
+            students[0] = null;
+        }
 
+
+        for (int i = 0; i < currentActiveStudentsIndex; i++) {
+
+            if (students[i] != null && fName.equals(students[i].getFirstName()) && lName.equals((students[i].getLastName()))) {
+                students[i] = null;
+
+            }
+        }
+
+        refreshArray();
     }
+
+
+    private void refreshArray() {
+
+        List<Student> list = new ArrayList<>();
+
+        for (Student student : students) {
+            if (student != null) {
+                list.add(student);
+            }
+        }
+
+        this.students = list.toArray(new Student[list.size()]);
+    }
+
+
+    public Student[] getStudentsByScore() {
+        refreshArray();
+
+        Student[] retVal = students;
+        Arrays.sort(retVal);
+
+        return retVal;
+    }
+
+    public HashMap<Student, String> getGradeBook() {
+        Integer currentStudents = getActiveStudentCountInClass();
+        Student[] sortedStudent = getStudentsByScore();
+        // double aveScore = getAverageExamScore();
+        HashMap<Student, String> retVal = new HashMap<>();
+        String grade = "";
+        for (int i = 1; i <= currentStudents; i++) {
+            grade = getGrade(i, currentStudents);
+            retVal.put(sortedStudent[i - 1], grade);
+        }
+
+        return retVal;
+    }
+
+    private String getGrade(int studentRank, Integer totalStudent) {
+        String retVal = "F";
+        double rank = (double) studentRank / totalStudent;
+        if (rank < 0.1) {
+            retVal = "A";
+        } else if (rank < 0.3) {
+            retVal = "B";
+        } else if (rank < 0.5) {
+            retVal = "C";
+        } else if (rank < 0.9) {
+            retVal = "D";
+        }
+
+        return retVal;
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 public class StudentTest {
 
     @Test
-    public void constructorTest(){
+    public void constructorTest() {
         // Given
         String expectedFirstName = "Bob";
         String expectedLastName = "Dole";
-        Double[] testScores = new Double[]{0.0, 13.0 , 66.5 };
+        Double[] testScores = new Double[]{0.0, 13.0, 66.5};
 
 
         // When
-        Student studentTest = new Student(expectedFirstName,expectedLastName, testScores);
+        Student studentTest = new Student(expectedFirstName, expectedLastName, testScores);
         String actualFName = studentTest.getFirstName();
         String actualLName = studentTest.getLastName();
 
@@ -25,10 +25,10 @@ public class StudentTest {
     }
 
     @Test
-    public void getSetFirstNameTest(){
+    public void getSetFirstNameTest() {
         // Given
         String expectedFirstName = "Not Me";
-        Student studentTest = new Student(null,null, null);
+        Student studentTest = new Student(null, null, null);
 
         // When
         studentTest.setFirstName(expectedFirstName);
@@ -40,10 +40,10 @@ public class StudentTest {
     }
 
     @Test
-    public void getSetLastNameTest(){
+    public void getSetLastNameTest() {
         // Given
         String expectedLasttName = "Not My Last Name";
-        Student studentTest = new Student(null,null, null);
+        Student studentTest = new Student(null, null, null);
 
         // When
         studentTest.setLastName(expectedLasttName);
@@ -55,11 +55,11 @@ public class StudentTest {
     }
 
     @Test
-    public void getNumberOfExamsTakenTest(){
+    public void getNumberOfExamsTakenTest() {
         // Given
-        Double[] testScores = new Double[]{0.0, 13.0 , 66.5 };
+        Double[] testScores = new Double[]{0.0, 13.0, 66.5};
         Integer expected = 3;
-        Student studentTest = new Student(null,null, testScores);
+        Student studentTest = new Student(null, null, testScores);
 
         // When
         Integer actual = studentTest.getNumberOfExamsTaken();
@@ -69,10 +69,10 @@ public class StudentTest {
     }
 
     @Test
-    public void getNumberOfExamsTakenNoExamsTest(){
+    public void getNumberOfExamsTakenNoExamsTest() {
         // Given
         Integer expected = 0;
-        Student studentTest = new Student(null,null, null);
+        Student studentTest = new Student(null, null, null);
 
         // When
         Integer actual = studentTest.getNumberOfExamsTaken();
@@ -82,15 +82,15 @@ public class StudentTest {
     }
 
     @Test
-    public void getExamScoreTest(){
+    public void getExamScoreTest() {
         // Given
         String firstName = "George";
         String lastName = "Bush";
-        Double[] examScores = { 100.00, 95.0, 123.0, 96.0};
+        Double[] examScores = {100.00, 95.0, 123.0, 96.0};
         Student student = new Student(firstName, lastName, examScores);
         String expected1 = "Exam Scores:\n\tExam 1 -> 100\n\tExam 2 -> 95\n";
         String expected2 = "\tExam 3 -> 123\n\tExam 4 -> 96\n";
-        String expected = expected1+expected2;
+        String expected = expected1 + expected2;
 
         //When
         String actual = student.getExamScores();
@@ -100,7 +100,7 @@ public class StudentTest {
     }
 
     @Test
-    public void getExamScoreNoExamsTest(){
+    public void getExamScoreNoExamsTest() {
         // Given
         Student student = new Student(null, null, null);
         String expected = "No Exams Taken";
@@ -116,7 +116,7 @@ public class StudentTest {
     public void addExamScoreTest() {
         // Given
         String expected = "Exam Scores:\n\tExam 1 -> 55\n";
-        Student student = new Student(null,null,null);
+        Student student = new Student(null, null, null);
 
         // When
         student.addExamScore(55.0);
@@ -129,37 +129,23 @@ public class StudentTest {
     @Test
     public void setExamScoreTest() {
         // Given
-        Double[] examScores = { 100.0 };
+        Double[] examScores = {100.0};
         String expected = "Exam Scores:\n\tExam 1 -> 99\n";
         Student student = new Student(null, null, examScores);
 
         // When
-        student.setExamScore(1,99.0);
+        student.setExamScore(1, 99.0);
         String actual = student.getExamScores();
 
         // Then
         Assert.assertEquals(expected, actual);
     }
-/* // if have time later - figure out expecting catch
-    @Test(expected = Exception.class)
-    public void setExamScoreIndexOutOfBoundTest() {
-        // Given
-        Double[] examScores = {100.0};
-        Student student = new Student(null, null, examScores);
 
-        // When
-        student.setExamScore(0, 99.0);
-        String actual = student.getExamScores();
-
-        // Then
-        // Exception thrown
-    }
-    */
 
     @Test
     public void getAverageExamScore() {
         // Given
-        Double[] examScores = {100.0 , 150.0, 250.0, 0.0};
+        Double[] examScores = {100.0, 150.0, 250.0, 0.0};
         Student student = new Student(null, null, examScores);
         Double expected = 125.0;
 
@@ -179,20 +165,20 @@ public class StudentTest {
         Double[] examScores = {100.0, 150.0, 250.0, 0.0};
         Student student = new Student(fName, lName, examScores);
         StringBuilder sb = new StringBuilder();
-        sb.append( "Student Name: David Tee\n");
-        sb.append( "Average Score: 125.0\n");
-        sb.append( "Exam Scores:\n");
-        sb.append( "\tExam 1 -> 100\n");
-        sb.append( "\tExam 2 -> 150\n");
-        sb.append( "\tExam 3 -> 250\n");
-        sb.append( "\tExam 4 -> 0 \n");
+        sb.append("Student Name: David Tee\n");
+        sb.append("Average Score: 125.0\n");
+        sb.append("Exam Scores:\n");
+        sb.append("\tExam 1 -> 100\n");
+        sb.append("\tExam 2 -> 150\n");
+        sb.append("\tExam 3 -> 250\n");
+        sb.append("\tExam 4 -> 0 \n");
         String expected = sb.toString();
 
         // When
         String actual = student.toString();
 
         // Then
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
 
 
